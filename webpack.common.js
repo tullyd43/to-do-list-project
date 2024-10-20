8c5b1da5b667c06d2d1d5f90ffc0133af023b9c8
tullyd43 <169837586+tullyd43@users.noreply.github.com>
2024-10-20 12:18:38 -0400

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+
+module.exports = {
+	entry: "./src/scripts/scripts.js",
+	output: {
+		filename: "main.js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Production",
+			template: "./src/index.html",
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.html$/i,
+				loader: "html-loader",
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+		],
+	},
+};
