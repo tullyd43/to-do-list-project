9aedb29679fbb87f67f0e86ccb177b9e26b78eb2
David Tully <169837586+tullyd43@users.noreply.github.com>
2024-10-20 14:43:59 -0400

    TaskGen prototype functions created

--- src/scripts/todo-creator.js
+++ src/scripts/todo-creator.js
@@ -3,19 +3,45 @@
 export const toDo = function() {
 	const toDoList = []
     class TaskGen {
-		constructor(title, description, dueDate, priority, notes, checklist) {
-			this.title = title
-			this.description = description
-			this.dueDate = dueDate
-			this.priority = priority
-			this.notes = notes
-			this.checklist = checklist
-        }
-    };
+		constructor(
+			title,
+			description,
+			dueDate,
+			priority,
+			notes,
+			checklist,
+			ID = TaskGen.createID()
+		) {
+			this.title = title;
+			this.description = description;
+			this.dueDate = dueDate;
+			this.priority = priority;
+			this.notes = notes;
+			this.checklist = checklist;
+			this.ID = ID;
+		}
+	};
+    TaskGen.createID = function() {
+        // Code here
+    }
     TaskGen.prototype.editTitle = function(title) {
         return (this.title = title)
     }
-
+    TaskGen.prototype.editDescription = function (description) {
+        return this.description = description
+    }
+    TaskGen.prototype.editDueDate = function (dueDate) {
+        return this.dueDate = dueDate
+    }
+    TaskGen.prototype.editPriority = function (priority) {
+        return this.priority = priority
+    }
+    TaskGen.prototype.editNotes = function (notes) {
+        return this.notes = notes
+    }
+    TaskGen.prototype.editChecklist = function (checklist) {
+        return this.checklist = checklist
+    }
     return {
         TaskGen,
         toDoList
